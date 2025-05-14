bbbb2e4a7ce2b531817c012f310f7159cb8fe5f3
Lorran N <42476753-gamerzinholl5@users.noreply.replit.com>
2025-05-14 15:38:57 +0000

    Restored to '41f0068ae7ea35ffb39be66dd8c972f6dd114ef7'
    Replit-Restored-To: 41f0068ae7ea35ffb39be66dd8c972f6dd114ef7

--- back-end/login_cadastro_aluno/compara.js
+++ back-end/login_cadastro_aluno/compara.js
@@ -1,10 +1,10 @@
 export function compara_dados(dados) {
   // implementar a função q vai buscar os dados no banco de dados
-    const usernameTeste = "teste@gmail.com";
-    const passwordTeste = "1234";
+    const usernameTeste = "admin@gmail.com";
+    const passwordTeste = "admin";
   if (dados.username === usernameTeste && dados.password === passwordTeste) {
       console.log("Login bem-sucedido!");
-      window.location.href = "../../site/pagina_principal/aluno/principal.html";
+      window.location.href = "../inicio/inicio.html";
   } else {
       console.log("Login ou senha incorretos.");
       console.log(username, password);
